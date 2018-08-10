b27b2ac7232b80ef375c9d757b5dafb5337bec2f
Malin Gernandt <malin.gernandt@frojd.se>
2018-08-10 14:17:03 +0200

    Add cupcake icon to counter

--- src/people/PeoplePage.js
+++ src/people/PeoplePage.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { ReactComponent as Cupcake } from '../images/cupcake-purple.svg'
 import TaskListing from '../components/TaskListing'
+
+const StyledCupcake = styled(Cupcake)`
+  width: 40px;
+  fill: #A800FF;
+  margin-left: 5px;
+`
+
+const Counter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 class PeoplePage extends React.Component {
   state = {
     count : 0
   }
   onItemClick = (points) => {
-    console.log('Item clicked: ', points);
     const {count} = this.state;
     this.setState({
       count: parseInt(count) + parseInt(points)
     })
   }
+
   render () {
     const { frontmatter } = this.props.data.markdownRemark
     return (
@@ -20,7 +35,10 @@ class PeoplePage extends React.Component {
         {frontmatter.tasks && frontmatter.tasks.map((item, index) =>
           <TaskListing key={index} item={item} onItemClick={this.onItemClick} />
         )}
-        <h2>{this.state.count}</h2>
+        <Counter>
+          <StyledCupcake />
+          <h2>{this.state.count}</h2>
+        </Counter>
       </div>
     )
   }
